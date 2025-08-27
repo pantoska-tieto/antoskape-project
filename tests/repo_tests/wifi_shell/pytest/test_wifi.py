35c4dc1256890115e1d88e67ac28dd3faa1a59f7
Peter Antoska <peter.antoska@tietoevry.com>
2025-08-27 22:25:24 +0200

    Use GITHUB_ENV file path in python script

--- tests/repo_tests/wifi_shell/pytest/test_wifi.py
+++ tests/repo_tests/wifi_shell/pytest/test_wifi.py
@@ -9,15 +9,21 @@ from twister_harness import DeviceAdapter
 import os
 
 # Initiate variables
-env_ssid = os.environ["SSID"]
-env_ssid_passwd = os.environ["SSID_PWD"]
+# Get the path of the runner file
+env_file = os.getenv("GITHUB_ENV") 
+with open(env_file, "r") as env_file:
+    env_content = env_file.readlines()
+    if any("SSID" in line for line in env_content):
+        env_ssid = os.environ["SSID"]
+    if any("SSID_PWD" in line for line in env_content):
+        env_ssid_passwd = os.environ["SSID_PWD"]
 print(f"pantoska SSID: {env_ssid}")
 
 
 @pytest.mark.dependency(name="scan")
 def test_wifi_scan(dut: DeviceAdapter, shell: Shell):
     print("Testcase: check available wifi SSIDs:")
-    shell.exec_command('wifi scan')
+    shell.exec_command("wifi scan")
     lines = dut.readlines_until("Scan request done", timeout=20)
     assert any(env_ssid.lower() in l.lower() for l in lines), "Scanning for demanded Wifi SSID failed!"    
 
@@ -25,7 +31,7 @@ def test_wifi_scan(dut: DeviceAdapter, shell: Shell):
 @pytest.mark.dependency(depends=["scan"])
 def test_wifi_connect(shell: Shell):
     print("Testcase: check successfull wifi connection:")
-    lines = shell.exec_command(f'wifi connect -s {env_ssid} -p {env_ssid_passwd} -k 1')
+    lines = shell.exec_command(f"wifi connect -s {env_ssid} -p {env_ssid_passwd} -k 1")
 
     # Check for wifi connection status in asynchronous returns from uart
     def wait_for_wifi_status(timeout=20, step=1) -> bool:
