ed0832bd003e67f0f15a4eea3ee35315d0e6f500
Peter Antoska <peter.antoska@tietoevry.com>
2025-09-05 08:20:27 +0200

    Fix tests path if test scenario is used

--- .github/scripts/get_tests_list.py
+++ .github/scripts/get_tests_list.py
@@ -25,28 +25,38 @@ def define_args():
     )
     return parser
 
+def get_test_paths(scope):
+    """Define CLI arguments set
+
+    :param: str scope: tag to point to test folder
+    :return: str scope: string to use in filename
+    :return: str target: real path to test folder
+    """
+    match args.tests_scope:
+        case "app/repo":
+            scope = "repo"
+            target = "tests/repo_tests"
+        case "app/integration":
+            scope = "integration"
+            target = "tests/integration_tests"
+        case "app/unit":
+            scope = "unit"
+            target = "tests/unit_tests"
+        case "app(all tests)":
+            scope = "application"
+            target = "tests"
+        case "zephyr(all tests)":
+            scope = "zephyr"
+            target = "../zephyr/tests"
+    return scope, target
+
+
 if __name__ == "__main__":
     parser = define_args()
     args = parser.parse_args()
     # Tests from specific folder are filtered (no scenario is specified)
     if args.tests_scope:
-        match args.tests_scope:
-            case "app/repo":
-                scope = "repo"
-                target = "tests/repo_tests"
-            case "app/integration":
-                scope = "integration"
-                target = "tests/integration_tests"
-            case "app/unit":
-                scope = "unit"
-                target = "tests/unit_tests"
-            case "app(all tests)":
-                scope = "application"
-                target = "tests"
-            case "zephyr(all tests)":
-                scope = "zephyr"
-                target = "../zephyr/tests"
-
+        scope, target = get_test_paths(args.tests_scope)
         # Get list of demanded tests
         test_list = [root for root, dirs, files in os.walk(target, topdown=True) for file in files if file == "prj.conf"]
         # Create demanded tests list in .txt file form
@@ -56,9 +66,10 @@ if __name__ == "__main__":
 
     # Specific test scenario is specified
     if args.tests_scenario:
+        scope, target = get_test_paths(args.tests_scenario)
         # Store option --testsuite-root to support test scenario filtering
         with open("scenario_tests.txt", "w") as f:
-            f.write(f"--testsuite-root {args.tests_scenario}\n")
+            f.write(f"--testsuite-root {target}\n")
 
 
     
