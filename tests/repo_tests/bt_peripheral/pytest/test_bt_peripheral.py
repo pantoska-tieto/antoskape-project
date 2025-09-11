ea805484880df60b47a396b6f32232c5787e4f67
Peter Antoska <peter.antoska@tietoevry.com>
2025-09-11 09:29:19 +0200

    Add hw support for bluetooth test

--- tests/repo_tests/bt_peripheral/pytest/test_bt_peripheral.py
+++ tests/repo_tests/bt_peripheral/pytest/test_bt_peripheral.py
@@ -5,12 +5,13 @@ import pytest
 import asyncio
 import pytest_asyncio
 from bleak import BleakScanner
+from twister_harness import DeviceAdapter
 
 
 logger = logging.getLogger(__name__)
 
-@pytest.mark.asyncio
-async def test_bluetooth_discovery():
+async def bluetooth_discovery(dut: DeviceAdapter):
+    await asyncio.sleep(5)
     devices = await BleakScanner.discover(10.0, return_adv=True)
     bt_name = []
     # Find BT device by name
@@ -18,5 +19,9 @@ async def test_bluetooth_discovery():
         advertisement_data = devices[device][1]
         bt_name.append(advertisement_data.local_name)
 
-    assert "TIETO Zephyr Peripheral" in bt_name, "Bluetooth device not found"
+    assert "TIETO Bluetooth" in bt_name
     logger.info('Bluetooth device found')
+
+def test_bt_discovery(dut: DeviceAdapter):
+        logger.info(f'Running Bluetooth discovery')
+        asyncio.run(bluetooth_discovery(dut))
