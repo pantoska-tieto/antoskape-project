1f83f0d65240e226155b142431d89d660388f95c
Peter Antoska <peter.antoska@tietoevry.com>
2025-09-12 18:24:33 +0200

    Update ztest gpio toggle with check-pin

--- tests/unit_tests/dut/gpio_toggle_ztest/src/main.c
+++ tests/unit_tests/dut/gpio_toggle_ztest/src/main.c
@@ -6,6 +6,7 @@
 
 #include <zephyr/ztest.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 
@@ -15,16 +16,17 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 // #define LED0_NODE DT_ALIAS(led0)
-#define SIZE 16
+#define SIZE 10
 
-#define LED_GPIO_CONTROLLER DT_NODELABEL(gpio0) // Use correct controller for your board
-#define LED_GPIO_PIN 13                          // Use correct pin number for your board
+/* The devicetree node identifier for the 
+// "led0" - control gpio pin and 
+// "led9 - pin to verify LED state
+// aliases. */
+#define LED0_NODE DT_ALIAS(led0)
+#define LED9_NODE DT_ALIAS(led9)
 
-static const struct gpio_dt_spec led = {
-    .port = DEVICE_DT_GET(LED_GPIO_CONTROLLER),
-    .pin = LED_GPIO_PIN,
-    .dt_flags = GPIO_ACTIVE_LOW,
-};
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led9 = GPIO_DT_SPEC_GET(LED9_NODE, gpios);
 
 /*
  * A build error on this line means your board is unsupported.
@@ -32,32 +34,67 @@ static const struct gpio_dt_spec led = {
  */
 // static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 int ret;
-bool led_state = false;
-bool expected_led_state = false;
+
+
+bool contains_false(const bool *list, int size) {
+    for (int i = 0; i < size; i++) {
+        if (!list[i]) {
+            return true;  // Found a false value
+        }
+    }
+    return false;  // All values are true
+}
+
 
 ZTEST(gpio_test, test_gpio_toggle_and_verify)
 {
     zassert_true(gpio_is_ready_dt(&led), "GPIO device not ready");
     printk("GPIO system is ready\n");
 
+    zassert_true(gpio_is_ready_dt(&led9), "GPIO device to verify LED state not ready");
+    printk("GPIO system to verify LED state is ready\n");
+
     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     printk("Initial Pin configure: %d\n", ret);
     zassert_true(ret == 0, "Failed to configure GPIO");
 
-    for (int i = 0; i < SIZE; i++) {
+    ret9 = gpio_pin_configure_dt(&led9, GPIO_INPUT);
+    printk("Initial Pin configure: %d\n", ret9);
+    zassert_true(ret9 == 0, "Failed to configure GPIO to verify LED state");
+
+    // Initial setup
+    bool *list = NULL;      // Start with an empty list of booleans
+    int size = 0;           // Current size of the list
+    int init_state = gpio_pin_get(led9.port, led9.pin);
+
+
+    for (int i = 0; i < SIZE + 1; i++) {
+        
+        bool *temp = realloc(list, (size + 1) * sizeof(bool));
+                if (temp == NULL) {
+                    free(list);
+                    printf("Memory allocation failed.\n");
+                    return 1;
+        }
+        list = temp;
+
+        // Toggle pin with LED connected
         ret = gpio_pin_toggle_dt(&led);
         zassert_true(ret == 0, "Failed to toggle GPIO");
-        // printk("led_state in cycle %d: %d\n", i, !led_state);
-        int actual_state = gpio_pin_get(led.port, led.pin);
-        zassert_true(actual_state >= 0, "Error reading GPIO state");
-        printk("Actual Pin status in cycle %d to: %d\n", i, actual_state);
 
-        // If LED is shining OFF/ON visually then test pass 
-        // gpio_pin_get() is not working for ESP32 boards - permanently reads 1
-        led_state = !led_state;
-        expected_led_state = !expected_led_state;
-        printk("LED state in cycle %d: %s\n", i, led_state ? "ON" : "OFF");
-        zassert_equal(expected_led_state, led_state, "GPIO (LED) state mismatch");
+        // Read pin state to verify LED state
+        int actual_state = gpio_pin_get(led9.port, led9.pin);
+        if (actual_state != init_state) {            
+            list[size] = true;
+            size++;
+            init_state = actual_state; // Update init_state to the new state
+        }
+        else {
+            list[size] = false;
+            size++;
+            init_state = actual_state;
+        }
+        zassert_equal(contains_false(*list, size), "GPIO (LED) state mismatch, test failed");
         k_msleep(SLEEP_TIME_MS);
     }
 }
