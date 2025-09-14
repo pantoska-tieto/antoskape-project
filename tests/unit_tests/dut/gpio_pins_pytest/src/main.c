83f3942046cb4701b290ea59a6d61ec3f80b8b8e
Peter Antoska <peter.antoska@tietoevry.com>
2025-09-14 21:28:21 +0200

    Add GPIO unit test with pytest

---
+++ tests/unit_tests/dut/gpio_pins_pytest/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025 Tietoevry
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int main(void)
+{
+	/* Shell application source code is injected by applied Kconfg SHELL
+	 * options, no more "extra" functionalities are required for exemplary
+	 * pytest test.
+	 */
+	return 0;
+}
