4b1870694017615a757bb077e08fce3f05be1b1a
Peter Antoska <peter.antoska@tietoevry.com>
2025-09-29 17:39:36 +0200

    Add auto mapping of serial ports to udev symlinks

---
+++ .github/scripts/udev_mapping_serial_ports.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Target board variable from GitHub dispatch event
+TARGET="$1"
+
+# Content of udev-rules file
+OUTPUT_FILE="../../udev_mapping.txt"
+> "$OUTPUT_FILE"
+
+if [ "$TARGET" == "esp32"* ]; then
+    VENDOR="esp32"
+    PORT="/dev/ttyUSB"
+elif [ "$TARGET" == "nrf"* ]; then
+    VENDOR="nrf"
+    PORT="/dev/ttyACM"
+else
+    echo "No vendor found for: $TARGET"
+    exit 1
+fi
+
+# Internal counter for multiple ports symlinks
+serial_counter=0
+
+# Loop through all ttyUSB devices
+for DEV in ${PORT}*; do
+    # Filter existing ports
+    [ -e "$DEV" ] || continue
+    echo "Processing serial port $DEV..."
+
+    # Get udev info
+    INFO=$(udevadm info -a -n "$DEV")
+
+    # Extract first occurrence of idVendor, idProduct (and serial)
+    ID_VENDOR=$(echo "$INFO" | grep -m 1 'ATTRS{idVendor}' | awk -F '=="' '{print $2}' | tr -d '"')
+    ID_PRODUCT=$(echo "$INFO" | grep -m 1 'ATTRS{idProduct}' | awk -F '=="' '{print $2}' | tr -d '"')
+    SERIAL=$(echo "$INFO" | grep -m 1 'ATTRS{serial}' | awk -F '=="' '{print $2}' | tr -d '"')
+
+    # echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"$ID_VENDOR\", ATTRS{idProduct}==\"$ID_PRODUCT\", SYMLINK+=\"$VENDOR\"" | sudo tee -a "$OUTPUT_FILE" > /dev/null
+    echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"$ID_VENDOR\", ATTRS{idProduct}==\"$ID_PRODUCT\", ATTRS{serial}==\"$SERIAL\", SYMLINK+=\"${VENDOR}_${$serial_counter}\"" | sudo tee "$RULE_FILE" > /dev/null
+
+    # Copy local file to new udev rule
+    sudo cp $OUTPUT_FILE /etc/udev/rules.d/99-$VENDOR.rules
+    # Reload udev rules
+    sudo udevadm control --reload-rules
+    sudo udevadm trigger
+    # Increment for further serial port
+    ((serial_counter++))
+    sleep 2
+done
+
+echo "Mapping udev finished and Device udev-rules saved to $OUTPUT_FILE"
