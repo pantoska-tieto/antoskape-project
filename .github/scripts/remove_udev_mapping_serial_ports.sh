304ab34481085e3d2cfb95d3e864dc86a7a76d8c
Peter Antoska <peter.antoska@tietoevry.com>
2025-09-30 08:24:49 +0200

    Add teardown for udev rules file

---
+++ .github/scripts/remove_udev_mapping_serial_ports.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# File with udev mapping data
+OUTPUT_FILE="udev_mapping.txt"
+
+# Check if the file exists
+if [[ -f "$OUTPUT_FILE" ]]; then
+    echo "Processing $OUTPUT_FILE..."
+    
+    while IFS= read -r line; do
+        # Parse name inside SYMLINK+="..." for udev file
+        symlink=$(echo "$line" | awk -F'SYMLINK\\+="' '{print $2}' | awk -F'_' '{print $1}')
+        if [[ -n "$symlink" ]]; then
+            echo "Executing: sudo rm /etc/udev/rules.d/99-$symlink.rules"
+            sudo rm "/etc/udev/rules.d/99-$symlink.rules"
+            sudo udevadm control --reload-rules
+			sudo udevadm trigger
+        fi
+    done < "$OUTPUT_FILE"
+
+    echo "Done."
+else
+    echo "File $OUTPUT_FILE not found."
+fi
