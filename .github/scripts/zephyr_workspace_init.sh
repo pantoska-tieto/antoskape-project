43719d559319887037281094b103296d61a6bc98
Peter Antoska <peter.antoska@tietoevry.com>
2025-10-28 10:18:03 +0100

    Remove typo in zephyr_workspace_init script

--- .github/scripts/zephyr_workspace_init.sh
+++ .github/scripts/zephyr_workspace_init.sh
@@ -6,13 +6,13 @@ cd /workspace/customer-application
 git config --global --add safe.directory "/workspace/zephyr"
 git config --global --add safe.directory "/workspace/modules/lib/uoscore-uedhoc"
 git config --global --add safe.directory "/workspace/modules/lib/zcbor"
-echo 'Initializing Zephyr environment...'
+echo "Initializing Zephyr environment..."
 pwd
-echo 'Current github workspace:'
+echo "Current github workspace:"
 ls -la
-echo 'Parent github folder'
+echo "Parent github folder"
 ls -la ..
 rm -rf ../.west
 west init -l .
 west update -o=--depth=1 -n
-west blobs fetch hal_espressif"
+west blobs fetch hal_espressif
