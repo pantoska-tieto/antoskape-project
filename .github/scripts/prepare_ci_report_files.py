9e3f3b4937f897720172dfe034412f30b30bc7ca
Peter Antoska <peter.antoska@tietoevry.com>
2025-10-29 14:30:06 +0100

    Fix wrong intendation in python script

--- .github/scripts/prepare_ci_report_files.py
+++ .github/scripts/prepare_ci_report_files.py
@@ -10,10 +10,10 @@ import shutil
 regex = re.compile('twister-out.*')
 for dir in os.listdir('.'):
     if regex.match(dir):
-    print(f"Found: {dir}")
-    if os.path.exists(os.path.join(dir, "twister_suite_report.xml")):
-        shutil.copyfile(os.path.join(dir, "twister_suite_report.xml"), \
-        os.path.join("test-summary", \
-        f"twister_suite_report{dir.split('-out')[1]}.xml"))
-    else:
-        print(f"File twister_suite_report.xml not found in {dir}")
+        print(f"Found: {dir}")
+        if os.path.exists(os.path.join(dir, "twister_suite_report.xml")):
+            shutil.copyfile(os.path.join(dir, "twister_suite_report.xml"), \
+            os.path.join("test-summary", \
+            f"twister_suite_report{dir.split('-out')[1]}.xml"))
+        else:
+            print(f"File twister_suite_report.xml not found in {dir}")
